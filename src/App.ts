2d95026bacfb39a83c91af6997876add3153cc5d
Matt Evans <matthew.e.evans@centene.com>
2020-07-29 16:54:55 -0500

    Added most of the mongo skeleton

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,14 @@
 import * as express from 'express';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
+
+const uri = `mongodb+srv://mattevans98:${process.env.MONGO_PASSWORD}@hob-data.netjl.mongodb.net/kreia-db?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+	const collection = client.db('kreia-db').collection('kreia-quotes');
+
+	client.close();
+});
 
 const app = express();
 const port = process.env.PORT || 8000;
